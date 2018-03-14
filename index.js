d144c400b50eff84f64a30b2a109960f49fbfc9a
Saikat Bhattacharya <saikat@Saikats-MacBook-Air.local>
2018-03-14 08:12:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/resume', (req, res) => {
+  res.sendFile(path.join(__dirname, '/resume.pdf'));
+});
+app.listen(port, () => {
+  console.log('App is running on port: ', port);
+})
