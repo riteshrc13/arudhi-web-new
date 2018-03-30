5fa8f35aa90ce7e5f24d8fb499fdb41f25b55405
Rahul Sharma <sharmarahul.ece@gmail.com>
2018-03-30 19:47:34 +0530

    Navigation Bar and Bulb-off final changes

--- content/js/main.js
+++ content/js/main.js
@@ -5,7 +5,7 @@ $(window).scroll(function(){
     var docH = $(document).height();
     var winH = $(window).height();
     var pct = (pos/docH);
-    var actPos = (pct * winH) - 400;
+    var actPos = (pct * winH) - 500;
     $('.light-bulb-off').animate({"top": actPos+"px"}, 1);
     $('#wct').text(pos);
     $('#dh').text(docH);
@@ -16,18 +16,28 @@ $(window).scroll(function(){
 ////////////////////////////////////////////////////////////////////////
 // Script: Navigation Animation
 $(document).ready(function(){
+        $('#nav-btn-cross').hide(1);
     $('#nav-btn').click(function(){
         //bring the navigation in
         var el = $('#hiddenNav');
-        var widt = $('#nav-btn').position().left - el.width() - $('#nav-btn').width() - 50; 
-        $('#hiddenNav').animate({"left":widt+"px"}, 500);
+            var widt = $('#nav-btn').position().left - el.width() - $('#nav-btn').width(); 
+            el.animate({"left":widt+"px"}, 1000);
+            $('#nav-btn').hide(1000);
+            $('#nav-btn-cross').show(1000);
     })
+
+    $('#nav-btn-cross').click(function() {
+        var ele = $('#hiddenNav');
+        ele.animate({"left":"100%"}, 1000);
+        $('#nav-btn').show(1000);
+        $('#nav-btn-cross').hide(1000);
+    });
 })
-$(document).ready(function(){
-    $('.navbar-brand').click(function(){
-        //bring the navigation in
-        $('#hiddenNav').animate({"left":"100%"}, 1000);
-    })
-})
+// $(document).ready(function(){
+//     $('.navbar-brand').click(function(){
+//         //bring the navigation in
+//         $('#hiddenNav').animate({"left":"100%"}, 1000);
+//     })
+// })
 
 ////////////////////////////////////////////////////////////////////////
