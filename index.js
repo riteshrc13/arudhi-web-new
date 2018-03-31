31a7ab083bdca6e1bcb6dee2b80697636cd432b1
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2018-04-01 00:29:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static('content'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
